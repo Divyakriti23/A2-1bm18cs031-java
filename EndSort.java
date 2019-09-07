6282e7886537bbf40e39b3dffaed4e37e7ead8fd
18nanma <42802604+18nanma@users.noreply.github.com>
2019-09-07 22:03:28 +0530

    end sort

--- EndSort.java
+++ EndSort.java
@@ -2,39 +2,67 @@ import java.util.*;
 class EndSort
 {
     String s;
-    int N;
+    int N, arr[], counter;
     
     void input()
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of elements");
         N = sc.nextInt();
+        arr = new int[N+N];
         System.out.println("Enter the numbers separated by a space");
         s = sc.next();
+        
     }
     
-    void output(String out)
+    void output()
     {
-        System.out.println("The sorted elements are:" + out);
+        System.out.println(counter);
+    }
+    //to convert string to array of numbers
+    void convert()
+    {
+        int i = 0;
+        char ele = '0';
+        while(ele != '\0')
+        {
+            ele = s.charAt(i);
+            if(ele != ' ')
+            {
+                arr[i] = ele;
+                
+            }
+            i++;
+        }
+            
     }
     
     void sort()
-    {   
-        int i = 0;
-        char ele = ' ';
-        while(ele != '\0'){
-            char ele = s.charAt(i);
-            if(ele == ' ')
+    {
+        for(int i=0; i<N; i++)
+        {
+            if(arr[i] > arr[i+1])
             {
-                i++;
-            }
-            else{
-                if (ele > s.charAt(i+2)){
-                    
-                }
+                arr[N] = arr[i];
+                counter++;
             }
         }
-        
-        
-    }
+    } 
 }
+
+class Main
+{
+    public static void main (String[] args) 
+    {
+         EndSort e = new EndSort();
+         e.input();
+         e.convert();
+         e.sort();
+         e.output();
+     }
+}
+        
+        
+        
+    
+
